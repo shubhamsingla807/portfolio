dc1d07557700f17f257c952cc2eead1bfa04796c
lovekesh <kumarmehlan@gmail.com>
2021-05-18 18:17:01 +0530

    navigator updated

--- js/project1.js
+++ js/project1.js
@@ -1,5 +1,6 @@
 window.onscroll = function () {
 	myFunction();
+	updateNavigator();
 };
 
 function myFunction() {
@@ -10,4 +11,46 @@ function myFunction() {
 		document.documentElement.clientHeight;
 	let scrolled = (winScroll / height) * 100;
 	document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+function checkVisible(elm) {
+	var rect = elm.getBoundingClientRect();
+	var viewHeight = Math.max(
+		document.documentElement.clientHeight,
+		window.innerHeight
+	);
+	return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
+let curr = "";
+
+function removePrevColor() {
+	let links = document.getElementById("navigator").getElementsByTagName("a");
+
+	for (let i = 0; i < links.length; i++) {
+		links[i].style.color = "black";
+	}
+}
+
+function updateNavigator() {
+	let links = document.getElementById("navigator").getElementsByTagName("a");
+	let sections = document.getElementsByTagName("section");
+	for (let i = 0; i < sections.length; i++) {
+		if (checkVisible(sections[i])) {
+			removePrevColor();
+			if (i == 0) {
+				links[0].style.color = "blue";
+			} else if (i == 1 || i == 2) {
+				links[1].style.color = "blue";
+			} else if (i == 3 || i == 4) {
+				links[2].style.color = "blue";
+			} else if (i == 5 || i == 6) {
+				links[3].style.color = "blue";
+			} else if (i == 7 || i == 8 || i == 9) {
+				links[4].style.color = "blue";
+			} else if (i == 10 || i == 11) {
+				links[5].style.color = "blue";
+			}
+		}
+	}
+	// links[0].style.color = "blue";
 }
